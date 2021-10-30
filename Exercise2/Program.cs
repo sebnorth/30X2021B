aa18fbacb06155410707e34208f0cc2d7ecef919
sebnorth <sebnorth@gmail.com>
2021-10-30 18:15:39 +0200

    Dodaj pliki projektów.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    /// <summary>
+    /// Exercise 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Uses ordered generic dynamic array
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            OrderedDynamicArray<Prostokat> tablica = new OrderedDynamicArray<Prostokat>();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercise2/Prostokat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Prostokat : IComparable
+    {
+
+        //int szerokosc;
+        //int wysokosc;
+
+        public int CompareTo(object obj) {
+
+            return 0;
+        
+        }
+    }
+}
